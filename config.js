ffd893733ae6aa8694abdfadfa18051d3de13992
bayonle2022 <lijadubayonle10@gmail.com>
2025-07-14 15:12:27 +0100

    Update API_KEY in config.js for correct authentication

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
     API_BASE_URL: "https://core.hisend.hunnovate.com/api/v1/projects",
     PROJECT_ID: "e-_commerce_powered_by_h_i-send",
-    API_KEY: "v2kLi82gZF8ROPq0bbkhILnK"
+    API_KEY: "qcrnHK2d88ewX9PmUYP8wyFK"
 }
